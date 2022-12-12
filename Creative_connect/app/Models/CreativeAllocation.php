8f7ac6433faa03bcfb3302f2a16a327d2f9022e0
cpurajesh666 <rajesh302502216009@gmail.com>
2022-12-12 17:33:55 +0530

    GetCreativeAllocation function optimization changes

--- Creative_connect/app/Models/CreativeAllocation.php
+++ Creative_connect/app/Models/CreativeAllocation.php
@@ -22,14 +22,13 @@ class CreativeAllocation extends Model
 	 */
     protected $fillable=['wrc_id', 'user_id', 'allocated_qty'];
 
+	//GetCreativeAllocation for view user detail
 	public static function GetCreativeAllocation(){
 
-		$resData = CreativeAllocation::orderBy('creative_allocation.id', 'DESC')
+		$resData = CreativeAllocation::orderBy('creative_allocation.user_id', 'DESC')
 		->leftJoin('users', 'users.id','creative_allocation.user_id')
-		->leftJoin('creative_wrc', 'creative_wrc.id','creative_allocation.wrc_id')
-		->leftJoin('creative_lots', 'creative_lots.id','creative_wrc.lot_id')
-		->select('creative_allocation.*','creative_wrc.wrc_number','creative_wrc.lot_id','creative_lots.lot_number','users.name',DB::raw('sum(creative_allocation.allocated_qty) as allocated_qty'))
-		->groupBy('creative_allocation.user_id','creative_allocation.wrc_id')
+		->select('creative_allocation.user_id','users.name')
+		->groupBy('creative_allocation.user_id')
 		->get();
 
 		$resDataManyUser = CreativeAllocation::orderBy('creative_allocation.user_id', 'DESC')
@@ -38,7 +37,6 @@ class CreativeAllocation extends Model
 		->leftJoin('creative_lots', 'creative_lots.id','creative_wrc.lot_id')
 		->select('creative_allocation.*','creative_wrc.wrc_number','creative_wrc.lot_id','creative_lots.lot_number','users.name')
 		->get();
-
 
 		return [
             "resData"         => $resData,
